65b0997d0cd559e246ec24a3ea1ba2b75d999eae
seongon Moon <seongon1010@gmail.com>
2026-09-21 15:09:16 +0900

    feat: add research portfolio page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.documentElement.dataset.app = "portfolio";
 
-const APP_VERSION = "v0.10.5";
+const APP_VERSION = "v0.11.0";
 
 window.portfolioElements = {
   menuButton: document.querySelector("[data-menu-button]"),
